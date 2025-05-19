571147f4039f68e0e693d8c9a23439e117e925fb
Harrison Pinto <hpintom@unal.edu.co>
2025-05-18 20:09:54 -0500

    Add hacky code to check for environment variables
    If `.env` file exist export all the variables within and then check for
    the existence of required environment variables.

---
+++ check_required_envs.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# HACK: Source .env file if it exists
+if [ -f .env ]; then
+  set -o allexport
+  source .env
+  set +o allexport
+fi
+
+required_vars=(
+  "REDIS_CONNECTION_STRING"
+  "JWT_SECRET"
+  "USER_TOKEN_EXPIRATION_TIME"
+  "DATABASE_URL"
+)
+
+missing_vars=0
+missing_vars_list=()
+
+for var in "${required_vars[@]}"; do
+  if [[ -z "${!var}" ]]; then
+    missing_vars_list+=("$var")
+    ((missing_vars++))
+  fi
+done
+
+if [[ $missing_vars -gt 0 ]]; then
+  >&2 echo "The following $missing_vars environment variables are missing:"
+  >&2 printf '%s\n' "${missing_vars_list[@]}"
+  exit 1
+fi
+
+echo "All required environment variables are set"
+exit 0
