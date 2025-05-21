fea42e4cbffebe5c9360d7d007013a50ba02e3ae
arojasag <arojasag@unal.edu.co>
2025-05-21 18:40:21 -0500

    Incorporate PR review suggestions and make service exclusive to unal members

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -12,9 +12,14 @@ router.post<{}, DataResponse, SignupRequest>('/signup', async (req, res, next) =
     const response: DataResponse = { data: null, errors: [] }
     const { email, password } = req.body as SignupRequest
 
-    //TODO: make sure only @unal.edu.co emails are allowed
     if (typeof email !== 'string' || typeof password !== 'string') {
         response.errors.push({ message: 'Invalid user information has been provided' })
+        res.status(400).json(response)
+        return
+    }
+    //MeetUN is meant to be an application exclusive to unal members
+    if (!email.toLowerCase().trim().endsWith("@unal.edu.co")) {
+        response.errors.push({ message: "Only @unal.edu.co emails are allowed" })
         res.status(400).json(response)
         return
     }
@@ -24,7 +29,7 @@ router.post<{}, DataResponse, SignupRequest>('/signup', async (req, res, next) =
 
     //TODO: improve error handling to inform that the email already has an account, if that's the case
     const user = await prisma.user.create({
-        data: { email: email.trim(), password: hashedPassword },
+        data: { email: email.toLowerCase().trim(), password: hashedPassword },
     }).catch( e => {
         console.error('Unexpected error creating user', e)
 
@@ -43,9 +48,8 @@ router.post<{}, DataResponse, SignupRequest>('/signup', async (req, res, next) =
     //  expiresIn: process.env.USER_TOKEN_EXPIRATION_TIME as string,
     //});
 
-    //TODO: add token to the response data
-    //TODO: don't return the hashed password. That's just being lazy
-    response.data = { ...user };
+    //TODO: add token to the response data    
+    response.data = user.id;
 
     res.status(201).json(response);
 })
