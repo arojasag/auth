4c8db919075eccd7acdc2da6407a1d932128e51a
arojasag <arojasag@unal.edu.co>
2025-06-02 01:14:47 -0500

    Change the auth/me endpoint so that it returns all user info (excluding password obviously)

--- src/routes/auth_me.ts
+++ src/routes/auth_me.ts
@@ -1,18 +1,37 @@
 import express from "express"
 import { DataResponse } from "../interfaces/interfaces"
 import { verifyToken } from "../middlewares/auth_middleware"
+import { PrismaClient } from "@prisma/client"
 
 const router = express.Router()
+const prisma = new PrismaClient()
 
 router.get<{}, DataResponse>('/auth/me', async (req, res) => {
 
-    verifyToken(req, res, () => {
+    verifyToken(req, res, async () => {
         const response: DataResponse = {data: null, errors: []}
-        const decodedToken = res.locals.decodedToken
-        const userId = decodedToken.id
-        response.data = {id: userId}
+        const userId = res.locals.decodedToken.id
+        
+        try {
+            const user = await prisma.user.findUnique({
+                where: { id: userId }
+            })
 
-        res.status(200).json(response)
+            if (user === null) {
+                response.errors.push({ message: `User with id ${userId} couldn't be found`})
+                return res.status(404).json(response)
+            }
+
+            // We return all user fields, excluding the hashed password
+            // There are so few fields that this can be done by hand and the microservice is so simple
+            // that this is unlikely to change
+            response.data = { id: user.id, email: user.email, isSuperuser: user.isSuperuser }
+            return res.status(200).json(response)
+        } catch (err) {
+            console.error(err)
+            response.errors.push({ message: "Unexpected error when trying to retrieve user"} )
+            res.status(500).json(response)
+        }
     })
 })
 
